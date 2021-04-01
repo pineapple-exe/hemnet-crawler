bf6be304eeec1c29c060f4212b117cff2872d0fa
Johanna Forssén <forssen.johanna@gmail.com>
2021-04-01 22:44:28 +0200

    Add initial database schema

---
+++ hemnet.sql
@@ -0,0 +1,24 @@
+create table listing (
+	id int not null,
+	[address] nvarchar not null,
+	price int not null,
+	[description] nvarchar not null,
+	home_type nvarchar not null,
+	type_of_ownership nvarchar not null,
+	rooms int null,
+	[living_area] int not null,
+	fee int null,
+	[bi_area] int null,
+	year_of_construction int null,
+	utilities int null,
+	number_of_visits int not null,
+	days_on_hemnet int not null
+	constraint PK_listing primary key (id)
+);
+
+create table [image] (
+	listing_id int not null,
+	image_data varbinary(max) not null,
+	content_type nvarchar not null,
+	foreign key (listing_id) references listing(id)
+);
