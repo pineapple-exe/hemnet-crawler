76321b54c84428a02b92d6dd5bc17216e2c87594
Johanna Forssén <forssen.johanna@gmail.com>
2021-04-11 20:32:27 +0200

    Only search for new listings

---
+++ HemnetCrawler.Data/Migrations/20210411182203_ShortenMaxLengthOnOwnershipType.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HemnetCrawler.Data.Migrations
+{
+    public partial class ShortenMaxLengthOnOwnershipType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "OwnershipType",
+                table: "Listings",
+                type: "nvarchar(22)",
+                maxLength: 22,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(11)",
+                oldMaxLength: 11);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "OwnershipType",
+                table: "Listings",
+                type: "nvarchar(11)",
+                maxLength: 11,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(22)",
+                oldMaxLength: 22);
+        }
+    }
+}
