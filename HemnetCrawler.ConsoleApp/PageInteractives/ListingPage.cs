834a1d55e5d202eb8a00791ab534938f8f98b4c5
Johanna Forssén <forssen.johanna@gmail.com>
2021-06-02 03:46:39 +0200

    Fix bug: Digit purify postalcode

--- HemnetCrawler.ConsoleApp/PageInteractives/ListingPage.cs
+++ HemnetCrawler.ConsoleApp/PageInteractives/ListingPage.cs
@@ -114,7 +114,7 @@ namespace HemnetCrawler.ConsoleApp
             Regex postalCodePattern = new Regex("(?<=\"postalCode\":\\s)\\d{3}\\s?\\d{2}");
             string postalCode = postalCodePattern.Match(driver.PageSource).Value;
             if (postalCode != "")
-               listing.PostalCode = int.Parse(postalCode);
+               listing.PostalCode = Utils.DigitPurist(postalCode);
 
             listing.Street = driver.FindElement(By.CssSelector("h1.qa-property-heading.hcl-heading.hcl-heading--size2")).Text;
             Thread.Sleep(1000);
