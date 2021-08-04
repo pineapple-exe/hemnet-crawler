5df00e88dc3674f29eb1265fe84f76964cc4d54b
Johanna Forssén <forssen.johanna@gmail.com>
2021-08-04 14:59:25 +0200

    Split HemnetCrawlerInteractor into multiple interactors

---
+++ HemnetCrawler.Domain/Interactors/FetchFinalBids.cs
@@ -0,0 +1,40 @@
+﻿using HemnetCrawler.Domain.Models;
+using HemnetCrawler.Domain.Repositories;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HemnetCrawler.Domain.Interactors
+{
+    public class FetchFinalBids
+    {
+        private readonly IFinalBidRepository _finalBidRepository;
+
+        public FetchFinalBids(IFinalBidRepository finalBidRepository)
+        {
+            _finalBidRepository = finalBidRepository;
+        }
+
+        public List<FinalBidOutputModel> ListFinalBids()
+        {
+            var allFinalBids = _finalBidRepository.GetAll().Take(100);
+
+            List<FinalBidOutputModel> outputModels = allFinalBids.Select(fb => new FinalBidOutputModel
+            {
+                Id = fb.Id,
+                Street = fb.Street,
+                City = fb.City,
+                PostalCode = fb.PostalCode,
+                Price = fb.Price,
+                SoldDate = fb.SoldDate,
+                DemandedPrice = fb.DemandedPrice,
+                PriceDevelopment = fb.PriceDevelopment,
+                HomeType = fb.HomeType,
+                Rooms = fb.Rooms,
+                LivingArea = fb.LivingArea,
+                Fee = fb.Fee
+            }).ToList();
+
+            return outputModels;
+        }
+    }
+}
---
+++ HemnetCrawler.Domain/Interactors/ListingQualities.cs
@@ -0,0 +1,74 @@
+﻿using HemnetCrawler.Domain.Entities;
+using HemnetCrawler.Domain.Models;
+using HemnetCrawler.Domain.Repositories;
+using System.Linq;
+
+
+namespace HemnetCrawler.Domain
+{
+    public class ListingQualities
+    {
+        private readonly IListingRepository _listingRepository;
+        private readonly IFinalBidRepository _finalBidRepository;
+        private readonly IListingRatingRepository _listingRatingRepository;
+
+        public byte[] GetImageData(int imageId)
+        {
+            byte[] imageData = _listingRepository.GetAllImages().First(img => img.Id == imageId).Data;
+
+            return imageData;
+        }
+
+        public double GetAveragePrice(int listingId)
+        {
+            Listing objectOfSpeculation = _listingRepository.GetAllListings().First(l => l.Id == listingId);
+
+            var relevantFinalBids = _finalBidRepository.GetAll().Where(fb => fb.HomeType == objectOfSpeculation.HomeType);
+
+            if (objectOfSpeculation.Rooms != null && relevantFinalBids.Where(fb => fb.Rooms == objectOfSpeculation.Rooms).Count() > 0)
+            {
+                relevantFinalBids = relevantFinalBids.Where(fb => fb.Rooms == objectOfSpeculation.Rooms);
+            }
+
+            if (relevantFinalBids.Where(fb => fb.City == objectOfSpeculation.City).Count() > 0)
+            {
+                relevantFinalBids = relevantFinalBids.Where(fb => fb.City == objectOfSpeculation.City);
+            }
+
+            if (objectOfSpeculation.PostalCode != null && relevantFinalBids.Where(fb => fb.PostalCode == objectOfSpeculation.PostalCode).Count() > 0)
+            {
+                relevantFinalBids = relevantFinalBids.Where(fb => fb.PostalCode == objectOfSpeculation.PostalCode);
+            }
+
+            double averagePrice = relevantFinalBids.Select(fb => fb.Price).Average();
+
+            return averagePrice;
+        }
+
+        public ListingRatingOutputModel GetListingRating(int listingId)
+        {
+            ListingRating relevantListingRating = _listingRatingRepository.GetAll().FirstOrDefault(lr => lr.ListingId == listingId);
+
+            if (relevantListingRating != null)
+            {
+                return new ListingRatingOutputModel(relevantListingRating.KitchenRating, relevantListingRating.BathroomRating);
+            }
+            else
+            {
+                return new ListingRatingOutputModel();
+            }
+        }
+
+        public void AddListingRating(int listingId, int kitchenRating, int bathroomRating)
+        {
+            ListingRating listingRating = new()
+            {
+                ListingId = listingId,
+                KitchenRating = kitchenRating,
+                BathroomRating = bathroomRating
+            };
+
+            _listingRatingRepository.AddListingRating(listingRating);
+        }
+    }
+}
