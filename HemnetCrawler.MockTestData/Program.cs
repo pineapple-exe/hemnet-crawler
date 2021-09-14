2ec27db529292e4d900258603378ddc70cd63836
Johanna Forssén <forssen.johanna@gmail.com>
2021-09-14 16:16:06 +0200

    Create MockTestData for cypress tests

---
+++ HemnetCrawler.MockTestData/Program.cs
@@ -0,0 +1,28 @@
+﻿using HemnetCrawler.Data;
+using HemnetCrawler.Data.Repositories;
+using HemnetCrawler.Domain.Entities;
+
+namespace HemnetCrawler.MockTestData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HemnetCrawlerDbContext context = new();
+
+            FinalBidRepository finalBidRepository = new(context);
+            ListingRepository listingRepository = new(context);
+
+            listingRepository.AddListing(new Listing()
+            {
+                Id = 1,
+                FinalBidId = 100
+            });
+
+            finalBidRepository.AddFinalBid(new FinalBid()
+            {
+                Id = 100
+            });
+        }
+    }
+}
