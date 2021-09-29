a17178e86944ffff220a9e6f1f0a0161740df1d4
Johanna Forssén <forssen.johanna@gmail.com>
2021-09-29 14:52:36 +0200

    Let MockTestData create database, run migrations and add mock data

--- HemnetCrawler.MockTestData/Program.cs
+++ HemnetCrawler.MockTestData/Program.cs
@@ -1,6 +1,8 @@
 ﻿using HemnetCrawler.Data;
 using HemnetCrawler.Data.Repositories;
 using HemnetCrawler.Domain.Entities;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 
 namespace HemnetCrawler.MockTestData
 {
@@ -10,6 +12,26 @@ namespace HemnetCrawler.MockTestData
         {
             HemnetCrawlerDbContext context = new();
 
+            CreateTestDatabaseWithSchema(context);
+            CreateMockData(context);
+        }
+
+        static void CreateTestDatabaseWithSchema(HemnetCrawlerDbContext context)
+        {
+            string sql = "CREATE DATABASE HemnetCrawlerTest";
+
+            using SqlConnection connection = new("Server=localhost;Database=master;Trusted_Connection=True;");
+
+            SqlCommand command = new(sql, connection);
+
+            command.Connection.Open();
+            command.ExecuteNonQuery();
+
+            context.Database.Migrate();
+        }
+
+        static void CreateMockData(HemnetCrawlerDbContext context)
+        {
             FinalBidRepository finalBidRepository = new(context);
             ListingRepository listingRepository = new(context);
 
