16740ac79a98fb625fa1884187e3337852fd1279
Johanna Forssén <forssen.johanna@gmail.com>
2021-11-16 17:51:48 +0100

    Create unit test for DeleteFinalBid, and start adjusting Listing_spec

---
+++ HemnetCrawler.Domain.Tests.Unit/DeleteFinalBidsTest.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+using HemnetCrawler.Domain.Entities;
+using HemnetCrawler.Domain.Interactors;
+using System.Linq;
+
+namespace HemnetCrawler.Domain.Tests.Unit
+{
+    public class DeleteFinalBidsTest
+    {
+        [Fact]
+        public void DeleteFinalBid_FinalBidWithListing_FinalBidAndFinalBidIdGone()
+        {
+            //Arrange
+            FakeFinalBidRepository finalBidRepository = new();
+            FakeListingRepository listingRepository = new();
+            DeleteFinalBids deleteFinalBids = new(finalBidRepository, listingRepository);
+
+            finalBidRepository.AddFinalBid(new FinalBid { Id = 1 });
+            listingRepository.AddListing(new Listing { Id = 100, FinalBidId = 1 });
+
+            //Act
+            deleteFinalBids.DeleteFinalBid(1);
+
+            //Assert
+            Assert.Empty(finalBidRepository.GetAll());
+
+            Listing listing = listingRepository.GetAllListings().Single();
+            Assert.Null(listing.FinalBidId);
+        }
+    }
+}
